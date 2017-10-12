9e561433a5fd12e45a3239c190fa4d48a03e405d
Adrien Cogny <ac2753@cornell.edu>
2017-10-12 18:32:25 -0400

    Calendar View Created (need to connect IB Outlets properly)

---
+++ CT-IBM-Cancer-App/CT-IBM-Cancer-App/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  CT-IBM-Cancer-App
+//
+//  Created by Adrien Cogny on 10/12/17.
+//  Copyright © 2017 Adrien Cogny. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dataLabel: UILabel!
+    
+}
--- CT-IBM-Cancer-App/CT-IBM-Cancer-App/ViewController.swift
+++ CT-IBM-Cancer-App/CT-IBM-Cancer-App/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import JTAppleCalendar
+
 
 class ViewController: UIViewController {
 
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +27,30 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "MyCustomCell", for: indexPath) as! CustomCell
+        
+        cell.dataLabel.text  = cellState.text
+        
+        return cell
+    }
+    
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        
+        formatter.dateFormat = "yyyy mm dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        
+        let startDate = formatter.date(from: "2017 10 01")!
+        let endDate = formatter.date(from: "2017 10 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate )
+        return parameters
+    }
+    
+}
