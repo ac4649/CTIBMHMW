3ba0158c718851f92cbcb5bea9b67218db3b92d5
Adrien Cogny <ac2753@cornell.edu>
2017-10-12 20:47:00 -0400

    Selection of day and colors are now defined in code (easily changeable).

--- CT-IBM-Cancer-App/CT-IBM-Cancer-App/ViewController.swift
+++ CT-IBM-Cancer-App/CT-IBM-Cancer-App/ViewController.swift
@@ -13,7 +13,21 @@ import JTAppleCalendar
 class ViewController: UIViewController {
     
     @IBOutlet weak var calView: JTAppleCalendarView!
-
+    
+    // color selection
+    //text colors
+    let selectedDayTextColor = UIColor(red: 255.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    let currentMonthTextColor = UIColor(red: 0.0, green: 0.0, blue: 255.0, alpha: 1.0)
+    let otherMonthTextColor = UIColor(red: 0.0, green: 100.0, blue: 100.0, alpha: 1.0)
+    
+    //view colors
+    let calendarBackground = UIColor(red:0.0, green:255.0, blue:0.0, alpha:1.0)
+    
+    
+    //
+    
+    
+    
     let formatter = DateFormatter()
     
     override func viewDidLoad() {
@@ -23,11 +37,50 @@ class ViewController: UIViewController {
         //remove the cell-spacing
         calView.minimumLineSpacing = 0
         calView.minimumInteritemSpacing = 0
+        calView.backgroundColor = calendarBackground
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState){
+        
+        guard let goodCell = view as? CustomCell else {return}
+        
+        if cellState.isSelected {
+            goodCell.dataLabel.textColor = selectedDayTextColor
+        }
+        else {
+            
+            if cellState.dateBelongsTo == .thisMonth {
+                goodCell.dataLabel.textColor = currentMonthTextColor
+                
+            }
+            else {
+                goodCell.dataLabel.textColor = otherMonthTextColor
+            }
+            
+        }
+        
+    }
+    
+    func handleCellSelection(view: JTAppleCell?, cellState: CellState){
+        
+        guard let goodCell = view as? CustomCell else { return }
+        
+        
+        if cellState.isSelected {
+            //cell is currently selected so show it
+            goodCell.selectedView.isHidden = false
+        }
+        else {
+            //not selected so hide it.
+            goodCell.selectedView.isHidden = true
+        }
+        
     }
 
 
@@ -55,25 +108,28 @@ extension ViewController: JTAppleCalendarViewDataSource {
 
 extension ViewController: JTAppleCalendarViewDelegate {
     
+    
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "MyCustomCell", for: indexPath) as! CustomCell
         
+
         cell.dataLabel.text  = cellState.text
         
+        handleCellSelection(view:cell, cellState:cellState)
+        handleCellTextColor(view:cell, cellState: cellState)
         return cell
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let goodCell = cell as? CustomCell else { return }
-        
-        goodCell.selectedView.isHidden = false
-        
+        handleCellSelection(view:cell, cellState:cellState)
+        handleCellTextColor(view:cell, cellState: cellState)
+
     }
     
     func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let goodCell = cell as? CustomCell else { return }
-        
-        goodCell.selectedView.isHidden = true
+        handleCellSelection(view:cell, cellState:cellState)
+        handleCellTextColor(view:cell, cellState: cellState)
+
     }
     
 }
